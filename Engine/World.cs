8617111a0690e9eb4901012cdcecf1af263a9077
Melinda Fischer <mfischer@tda.edu.au>
2021-01-21 15:05:14 +1100

    added World class now up to lesson 12.1

--- Engine/World.cs
+++ Engine/World.cs
@@ -4,7 +4,231 @@ using System.Text;
 
 namespace Engine
 {
-    class World
+    public static class World
     {
+        /* Lines 14 – 17: Static list variables. 
+         * These work similar to the properties in a class. 
+         * We’ll populate them with all the things in our game world, 
+         * then read from them in the rest of the program.*/
+
+        public static readonly List<Item> Items = new List<Item>();
+        public static readonly List<Monster> Monsters = new List<Monster>();
+        public static readonly List<Quest> Quests = new List<Quest>();
+        public static readonly List<Location> Locations = new List<Location>();
+
+        public const int ITEM_ID_RUSTY_SWORD = 1;
+        public const int ITEM_ID_RAT_TAIL = 2;
+        public const int ITEM_ID_PIECE_OF_FUR = 3;
+        public const int ITEM_ID_SNAKE_FANG = 4;
+        public const int ITEM_ID_SNAKESKIN = 5;
+        public const int ITEM_ID_CLUB = 6;
+        public const int ITEM_ID_HEALING_POTION = 7;
+        public const int ITEM_ID_SPIDER_FANG = 8;
+        public const int ITEM_ID_SPIDER_SILK = 9;
+        public const int ITEM_ID_ADVENTURER_PASS = 10;
+
+        public const int MONSTER_ID_RAT = 1;
+        public const int MONSTER_ID_SNAKE = 2;
+        public const int MONSTER_ID_GIANT_SPIDER = 3;
+
+        public const int QUEST_ID_CLEAR_ALCHEMIST_GARDEN = 1;
+        public const int QUEST_ID_CLEAR_FARMERS_FIELD = 2;
+
+        public const int LOCATION_ID_HOME = 1;
+        public const int LOCATION_ID_TOWN_SQUARE = 2;
+        public const int LOCATION_ID_GUARD_POST = 3;
+        public const int LOCATION_ID_ALCHEMIST_HUT = 4;
+        public const int LOCATION_ID_ALCHEMISTS_GARDEN = 5;
+        public const int LOCATION_ID_FARMHOUSE = 6;
+        public const int LOCATION_ID_FARM_FIELD = 7;
+        public const int LOCATION_ID_BRIDGE = 8;
+        public const int LOCATION_ID_SPIDER_FIELD = 9;
+
+        /* Static constructor, when the game starts the constructor method will 
+         * be run and the lists will get populated*/
+        static World()
+        {
+            PopulateItems();
+            PopulateMonsters();
+            PopulateQuests();
+            PopulateLocations();
+        }
+        
+        // methods to create the game objects and add them to the static lists
+        
+        private static void PopulateItems()
+        {
+            Items.Add(new Weapon(ITEM_ID_RUSTY_SWORD, "Rusty sword", "Rusty swords", 0, 5));
+            Items.Add(new Item(ITEM_ID_RAT_TAIL, "Rat tail", "Rat tails"));
+            Items.Add(new Item(ITEM_ID_PIECE_OF_FUR, "Piece of fur", "Pieces of fur"));
+            Items.Add(new Item(ITEM_ID_SNAKE_FANG, "Snake fang", "Snake fangs"));
+            Items.Add(new Item(ITEM_ID_SNAKESKIN, "Snakeskin", "Snakeskins"));
+            Items.Add(new Weapon(ITEM_ID_CLUB, "Club", "Clubs", 3, 10));
+            Items.Add(new HealingPotion(ITEM_ID_HEALING_POTION, "Healing potion", "Healing potions", 5));
+            Items.Add(new Item(ITEM_ID_SPIDER_FANG, "Spider fang", "Spider fangs"));
+            Items.Add(new Item(ITEM_ID_SPIDER_SILK, "Spider silk", "Spider silks"));
+            Items.Add(new Item(ITEM_ID_ADVENTURER_PASS, "Adventurer pass", "Adventurer passes"));
+        }
+
+        private static void PopulateMonsters()
+        {
+            Monster rat = new Monster(MONSTER_ID_RAT, "Rat", 5, 3, 10, 3, 3);
+            rat.LootTable.Add(new LootItem(ItemByID(ITEM_ID_RAT_TAIL), 75, false));
+            rat.LootTable.Add(new LootItem(ItemByID(ITEM_ID_PIECE_OF_FUR), 75, true));
+
+            Monster snake = new Monster(MONSTER_ID_SNAKE, "Snake", 5, 3, 10, 3, 3);
+            snake.LootTable.Add(new LootItem(ItemByID(ITEM_ID_SNAKE_FANG), 75, false));
+            snake.LootTable.Add(new LootItem(ItemByID(ITEM_ID_SNAKESKIN), 75, true));
+
+            Monster giantSpider = new Monster(MONSTER_ID_GIANT_SPIDER, "Giant spider", 20, 5, 40, 10, 10);
+            giantSpider.LootTable.Add(new LootItem(ItemByID(ITEM_ID_SPIDER_FANG), 75, true));
+            giantSpider.LootTable.Add(new LootItem(ItemByID(ITEM_ID_SPIDER_SILK), 25, false));
+
+            Monsters.Add(rat);
+            Monsters.Add(snake);
+            Monsters.Add(giantSpider);
+        }
+
+        private static void PopulateQuests()
+        {
+            Quest clearAlchemistGarden =
+                new Quest(
+                    QUEST_ID_CLEAR_ALCHEMIST_GARDEN,
+                    "Clear the alchemist's garden",
+                    "Kill rats in the alchemist's garden and bring back 3 rat tails. You will receive a healing potion and 10 gold pieces.", 20, 10);
+
+            clearAlchemistGarden.QuestCompletionItems.Add(new QuestCompletionItem(ItemByID(ITEM_ID_RAT_TAIL), 3));
+
+            clearAlchemistGarden.rewardItem = ItemByID(ITEM_ID_HEALING_POTION);
+
+            Quest clearFarmersField =
+                new Quest(
+                    QUEST_ID_CLEAR_FARMERS_FIELD,
+                    "Clear the farmer's field",
+                    "Kill snakes in the farmer's field and bring back 3 snake fangs. You will receive an adventurer's pass and 20 gold pieces.", 20, 20);
+
+            clearFarmersField.QuestCompletionItems.Add(new QuestCompletionItem(ItemByID(ITEM_ID_SNAKE_FANG), 3));
+
+            clearFarmersField.rewardItem = ItemByID(ITEM_ID_ADVENTURER_PASS);
+
+            Quests.Add(clearAlchemistGarden);
+            Quests.Add(clearFarmersField);
+        }
+
+        private static void PopulateLocations()
+        {
+            // Create each location
+            Location home = new Location(LOCATION_ID_HOME, "Home", "Your house. You really need to clean up the place.");
+
+            Location townSquare = new Location(LOCATION_ID_TOWN_SQUARE, "Town square", "You see a fountain.");
+
+            Location alchemistHut = new Location(LOCATION_ID_ALCHEMIST_HUT, "Alchemist's hut", "There are many strange plants on the shelves.");
+            alchemistHut.questAvailableHere = QuestByID(QUEST_ID_CLEAR_ALCHEMIST_GARDEN);
+
+            Location alchemistsGarden = new Location(LOCATION_ID_ALCHEMISTS_GARDEN, "Alchemist's garden", "Many plants are growing here.");
+            alchemistsGarden.monsterLivingHere = MonsterByID(MONSTER_ID_RAT);
+
+            Location farmhouse = new Location(LOCATION_ID_FARMHOUSE, "Farmhouse", "There is a small farmhouse, with a farmer in front.");
+            farmhouse.questAvailableHere = QuestByID(QUEST_ID_CLEAR_FARMERS_FIELD);
+
+            Location farmersField = new Location(LOCATION_ID_FARM_FIELD, "Farmer's field", "You see rows of vegetables growing here.");
+            farmersField.monsterLivingHere = MonsterByID(MONSTER_ID_SNAKE);
+
+            Location guardPost = new Location(LOCATION_ID_GUARD_POST, "Guard post", "There is a large, tough-looking guard here.", ItemByID(ITEM_ID_ADVENTURER_PASS));
+
+            Location bridge = new Location(LOCATION_ID_BRIDGE, "Bridge", "A stone bridge crosses a wide river.");
+
+            Location spiderField = new Location(LOCATION_ID_SPIDER_FIELD, "Forest", "You see spider webs covering covering the trees in this forest.");
+            spiderField.monsterLivingHere = MonsterByID(MONSTER_ID_GIANT_SPIDER);
+
+            // Link the locations together
+            home.locationToNorth = townSquare;
+
+            townSquare.locationToNorth = alchemistHut;
+            townSquare.locationToSouth = home;
+            townSquare.locationToEast = guardPost;
+            townSquare.locationToWest = farmhouse;
+
+            farmhouse.locationToEast = townSquare;
+            farmhouse.locationToWest = farmersField;
+
+            farmersField.locationToEast = farmhouse;
+
+            alchemistHut.locationToSouth = townSquare;
+            alchemistHut.locationToNorth = alchemistsGarden;
+
+            alchemistsGarden.locationToSouth = alchemistHut;
+
+            guardPost.locationToEast = bridge;
+            guardPost.locationToWest = townSquare;
+
+            bridge.locationToWest = guardPost;
+            bridge.locationToEast = spiderField;
+
+            spiderField.locationToWest = bridge;
+
+            // Add the locations to the static list
+            Locations.Add(home);
+            Locations.Add(townSquare);
+            Locations.Add(guardPost);
+            Locations.Add(alchemistHut);
+            Locations.Add(alchemistsGarden);
+            Locations.Add(farmhouse);
+            Locations.Add(farmersField);
+            Locations.Add(bridge);
+            Locations.Add(spiderField);
+        }
+
+        // these methods get values from the static lists by using the constants 
+        public static Item ItemByID(int id)
+        {
+            foreach (Item item in Items)
+            {
+                if (item.id == id)
+                {
+                    return item;
+                }
+            }
+
+            return null;
+        }
+        public static Monster MonsterByID(int id)
+        {
+            foreach (Monster monster in Monsters)
+            {
+                if (monster.id == id)
+                {
+                    return monster;
+                }
+            }
+
+            return null;
+        }
+
+        public static Quest QuestByID(int id)
+        {
+            foreach (Quest quest in Quests)
+            {
+                if (quest.id == id)
+                {
+                    return quest;
+                }
+            }
+
+            return null;
+        }
+
+        public static Location LocationByID(int id)
+        {
+            foreach (Location location in Locations)
+            {
+                if (location.id == id)
+                {
+                    return location;
+                }
+            }
+
+            return null;
+        }
     }
 }
